1c4737dbc09af37524b916b7eaeacd02149f0deb
msfahim91 <msfahim71291@gmail.com>
2026-05-02 02:14:33 +0600

    Fix postcss config for Vercel deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'inventory-management-system-production-e012.up.railway.app',
 });
 
 API.interceptors.request.use((config) => {
