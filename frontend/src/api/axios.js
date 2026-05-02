48cba8121108c97d10e941b4ba97b462450f3dc6
msfahim91 <msfahim71291@gmail.com>
2026-05-03 03:40:48 +0600

    Fix backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const API = axios.create({
   // baseURL: 'http://localhost:8080/api',
-  baseURL: 'inventory-management-system-production-e012.up.railway.app',
+  // baseURL: 'inventory-management-system-production-e012.up.railway.app',
+  baseURL: 'https://inventory-management-system-production-e012.up.railway.app/api',
 });
 
 API.interceptors.request.use((config) => {
